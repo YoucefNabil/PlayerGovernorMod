5538ca7d18e21e5087f1acaad85e2b9f593576d9
Youcef <37760798+YoucefNabil@users.noreply.github.com>
2026-01-03 22:28:18 +0100

    1st commit (idk what I'm doing)

---
+++ SubModule.cs
@@ -0,0 +1,24 @@
+﻿using TaleWorlds.CampaignSystem;
+using TaleWorlds.Core;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.ObjectSystem;
+using TaleWorlds.Library;
+using TaleWorlds.Localization;
+
+namespace PlayerGovernorMod
+{
+    public class SubModule : MBSubModuleBase
+    {
+        protected override void OnGameStart(Game game, IGameStarter gameStarterObject)
+        {
+            base.OnGameStart(game, gameStarterObject);
+
+            // Ensure we are only adding this to the Campaign mode (not Custom Battle)
+            if (gameStarterObject is CampaignGameStarter campaignStarter)
+            {
+                // Register your behavior class here
+                campaignStarter.AddBehavior(new PlayerGovernorBehavior());
+            }
+        }
+    }
+}
